142de90b93b41a32535d4030b58a9dd218f58cca
RichardMiruka <richardonsare15@gmail.com>
2025-10-22 00:23:47 +0300

    added an explanation to the example

--- data_types/object_data_type.js
+++ data_types/object_data_type.js
@@ -6,3 +6,16 @@ let book = {
 	author: "F. Scott Fitzgerald",
 	year: 1925
 };
+
+console.log(book.title);
+console.log(book.author);
+
+
+
+// Methods = Functions inside Objects
+// getSummary: function() {
+// console.log(`${this.title} was written by ${this.author} in ${this.year}.`);
+// }
+
+
+// book.getSummary();
