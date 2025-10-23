5fe070112271395769f636c52aaead50d5bcac4b
RichardMiruka <richardonsare15@gmail.com>
2025-10-23 14:58:59 +0300

    decalred variable using var and commeneted it out

---
+++ teaching_examples/main.js
@@ -0,0 +1,10 @@
+/*/ Declaring Variables
+var (old way)
+let (modern, flexible way of declaring variables)
+const  ( for constants - don't change their value)*/ 
+
+/*var person = "Richard";
+var person = "Harry"; // Re-declaring a variable with var is allowed
+
+console.log(person); // Output: Harry */
+
