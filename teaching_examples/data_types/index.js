f4298e67524333138d971e533b11d1cc97e58866
RichardMiruka <richardonsare15@gmail.com>
2025-10-23 15:45:10 +0300

    example code of primitive data types

---
+++ teaching_examples/data_types/index.js
@@ -0,0 +1,18 @@
+// Primitive Data Types examples in JavaScript
+let firstName = "John";
+let age = 30;
+let isStudent = true;
+let middleName;
+let lastName = "Doe";
+let car =   null;
+let uniqueId = Symbol("id");
+let bigNumber = 9007199254740991n;
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof middleName);
+console.log(typeof lastName);
+console.log(typeof car);
+console.log(typeof uniqueId);
+console.log(typeof bigNumber);
