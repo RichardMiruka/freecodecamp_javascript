2750cbd5d6e1fb86aa44461f193563c19ba4f036
RichardMiruka <richardonsare15@gmail.com>
2025-10-26 17:38:49 +0300

    strict inequality and equality operators

---
+++ learning_examples/operators/inequality_operator.js
@@ -0,0 +1,7 @@
+// checking is something is not equal to another value
+
+console.log(5 != "5);
+
+// the result would be false because the inequality operator first converts 
+// the string value to a number and then compares the values
+// Since the values would be the same it will return false
---
+++ learning_examples/operators/strict_inequality_operator.js
@@ -0,0 +1,9 @@
+// strict inequality operator ( !== )
+
+console.log(5 !== "5");
+
+/*
+ * The result would be true because the strict inequality operator 
+ * does not perform any type coercion. 
+ * Since the number 5 is not equal to the string "5", then the result is true.
+*/
