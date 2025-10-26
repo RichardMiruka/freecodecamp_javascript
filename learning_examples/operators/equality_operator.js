7f2d7995bfccb3c423591ec5b77857daf31fe154
RichardMiruka <richardonsare15@gmail.com>
2025-10-26 17:11:55 +0300

    type coercion in JavaScript

---
+++ learning_examples/operators/equality_operator.js
@@ -0,0 +1,7 @@
+// Type coercion done here.
+
+console.log(5 == "5");
+
+//JavaScript converts the string "5" into the number 5 and then checks if they are equal.
+// Since both values are now the same, the result is true. 
+// The equality operator uses type coercion before checking if each value is equal.
