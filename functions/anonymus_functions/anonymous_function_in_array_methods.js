6626547276e28e2d06e30b85ff176b2422ff4528
RichardMiruka <richardonsare15@gmail.com>
2025-11-01 17:03:24 +0300

    working on anonymous functions

---
+++ functions/anonymus_functions/anonymous_function_in_array_methods.js
@@ -0,0 +1,15 @@
+const fruits = ["apple", "mango", "banana"];
+
+fruits.forEach(function(fruit) {
+	console.log("Fruit:", fruit);
+});
+
+// map 
+const numbers = [1, 2, 3, 4];
+
+const doubled = numbers.map(function(num) {
+  return num * 2;
+});
+
+console.log(doubled); // [2,4,6,8]
+
---
+++ functions/named_functions/repeats_greeting_function.js
@@ -0,0 +1,7 @@
+function greetMany(name, times) {
+	for (let i = 0; i < times; i++) {
+	console.log("Hello " + name);
+	}
+}
+
+greetMany("Melissa", 4);
