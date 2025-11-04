d71c2924658cb5af41bae118f3f5648b2a29a0d3
RichardMiruka <richardonsare15@gmail.com>
2025-11-04 22:28:31 +0300

    anonymous functions in JavaScript

---
+++ functions/anonymus_functions/anonymous_function_as_object_method.js
@@ -0,0 +1,8 @@
+const person = {
+	name: "Sarah",
+	speak: function () {
+	 console.log("Hi, I'm " + this.name);
+	},
+};
+
+person.speak();
---
+++ functions/anonymus_functions/challenge_anonymous_function.js
@@ -0,0 +1,6 @@
+const greet = function (name="User") {
+	return "Welcome, " + name + "!";
+};
+
+console.log(greet());
+console.log(greet("Sam"));
