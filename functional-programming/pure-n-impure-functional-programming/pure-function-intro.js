66df930a6adcad9ef26e9e8df7c1d206d4434f8d
RichardMiruka <richardonsare15@gmail.com>
2026-02-15 07:34:55 +0300

    feat: created multiple code examples to demo the subject

---
+++ functional-programming/pure-n-impure-functional-programming/pure-function-intro.js
@@ -0,0 +1,20 @@
+ /* Functional programming is about building programs from small, pure, predictable functions and keeping side effects separate.
+  * A function is pure if:
+  * Same input → Same output - always predictable.
+  * It does NOT change anything outside itself
+*/
+
+function add(a,b) { 
+	return a + b;
+}
+
+console.log(add(2, 4));
+
+
+
+// example 2
+function multiply(c,d) {
+	return c * d;
+}
+console.log(multiply(8, 7));
+console.log(multiply(10, 20));
